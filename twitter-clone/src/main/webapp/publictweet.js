0d60073e7c556403703872ef3e560288a6b38473
zedongzhang <zedongzhang@yahoo.com>
2016-08-05 16:46:23 -0400

    Update

--- twitter-clone/src/main/webapp/publictweet.js
+++ twitter-clone/src/main/webapp/publictweet.js
@@ -1,16 +1,19 @@
 function getPulicTweet() {
-	var jsonText = [{formUserID:'ABC',toUserID:'BCD',msg:'Test form ABC to BCD',date:'2016-08-05 12:00:01'},{formUserID:'ASD',toUserID:'ZXC',msg:'Test form ASD to ZXC',date:'2016-08-05 12:00:02'}];
-	
-	//var publictweetArr = JSON.parse(jsonText);
 
-	var publictweet = document.getElementById("publictweet");
-	var htmlStr = "";
-	for(var i in jsonText) {
-		htmlStr += "<div class='tweetItem'>";
-		htmlStr += "<div><span class='tweetUserID'>" + jsonText[i]["formUserID"] + "</span><span class='tweetDate'>" + jsonText[i]["date"] + "</span></div>";
-		htmlStr += "<div class='tweetMsg'>" + jsonText[i]["msg"] + "</div>";
-		htmlStr += "</div>";
-	}
-	publictweet.innerHTML = htmlStr;
-	
+	ajaxHandler("Rest/userinfo/", function (jsonText) {
+
+		//var jsonText = [{formUserID:'ABC',toUserID:'BCD',msg:'Test form ABC to BCD',date:'2016-08-05 12:00:01'},{formUserID:'ASD',toUserID:'ZXC',msg:'Test form ASD to ZXC',date:'2016-08-05 12:00:02'}];
+		
+		//var publictweetArr = JSON.parse(tweets);
+
+		var publictweet = document.getElementById("publictweet");
+		var htmlStr = "";
+		for(var i in jsonText) {
+			htmlStr += "<div class='tweetItem'>";
+			htmlStr += "<div><span class='tweetUserID'>" + jsonText[i]["fromUserId"] + "</span><span class='tweetDate'>" + jsonText[i]["date"] + "</span></div>";
+			htmlStr += "<div class='tweetMsg'>" + jsonText[i]["msg"] + "</div>";
+			htmlStr += "</div>";
+		}
+		publictweet.innerHTML = htmlStr;
+	});
 }
