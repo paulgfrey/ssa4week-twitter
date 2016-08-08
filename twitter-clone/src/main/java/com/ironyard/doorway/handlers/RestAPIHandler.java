0728a3f48661056a86290d8d935d479f5c84d7ff
paulgfrey <paulgfrey@gmail.com>
2016-08-08 09:43:56 -0400

    fixed JSON to use GSON for rendering

--- twitter-clone/src/main/java/com/ironyard/doorway/handlers/RestAPIHandler.java
+++ twitter-clone/src/main/java/com/ironyard/doorway/handlers/RestAPIHandler.java
@@ -1,7 +1,7 @@
 package com.ironyard.doorway.handlers;
 
 import java.io.IOException;
-import java.sql.Date;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,12 +10,16 @@ import javax.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.ironyard.doorway.domain.Tweet;
 import com.ironyard.doorway.domain.User;
 
 public class RestAPIHandler extends AbstractHandler {
-
+	private Gson gson = null;
+	
 	public RestAPIHandler() {
+		gson = new GsonBuilder().create();
 	}
 
 	@Override
@@ -43,6 +47,8 @@ public class RestAPIHandler extends AbstractHandler {
 	private void getFollowers(String target, HttpServletRequest request,
 			HttpServletResponse response) throws IOException {
 
+		String userId = Utils.getUserId(request);
+		
 		// Get User ID
 		int ndx = target.lastIndexOf("/");
 		if (ndx == -1) {
@@ -55,16 +61,18 @@ public class RestAPIHandler extends AbstractHandler {
 
 		// TODO get User Info from followerDAO.
 
-		User user = new User();
-		user.setUserId("Follower");
-		user.setPassword("password");
-		user.setFirstName("Jane");
-		user.setLastName("Doe");
+		User[] users = new User[1];
+		users[0] = new User();
+		users[0].setUserId("Follower");
+		users[0].setPassword("password");
+		users[0].setFirstName("Jane");
+		users[0].setLastName("Doe");
+		
 
 		// Generate JSON
-		response.getWriter().println("[ " + user + " ]");
+		response.getWriter().println(gson.toJson(users));
 
-		Utils.setUserId(response, user.getUserId());
+		Utils.setUserId(response, userId);
 	}
 
 	private void getTweets(String target, HttpServletRequest request,
@@ -82,16 +90,20 @@ public class RestAPIHandler extends AbstractHandler {
 		tweet1.setToUserId(userId);
 		tweet1.setFromUserId("Follower");
 		tweet1.setMsg("This is a test tweet!");
-		tweet1.setDate(new Date(2016, 8, 5));
+		tweet1.setDate(new Date());
 		
 		Tweet tweet2 = new Tweet();
 		tweet2.setToUserId(userId);
 		tweet2.setFromUserId("Follower");
 		tweet2.setMsg("This is a test tweet!");
-		tweet2.setDate(new Date(2016, 8, 1));
+		tweet2.setDate(new Date());
+		
+		Tweet[] tweets = new Tweet[2];
+		tweets[0] = tweet1;
+		tweets[1] = tweet2;
 
 		// Generate JSON
-		response.getWriter().println("[ " + tweet1  + ", " + tweet2 + " ]");
+		response.getWriter().println(gson.toJson(tweets));
 
 		Utils.setUserId(response, userId);
 	}
@@ -118,7 +130,7 @@ public class RestAPIHandler extends AbstractHandler {
 		user.setLastName("Doe");
 
 		// Generate JSON
-		response.getWriter().println("[ " + user + " ]");
+		response.getWriter().println(gson.toJson(user));
 
 		Utils.setUserId(response, user.getUserId());
 	}
