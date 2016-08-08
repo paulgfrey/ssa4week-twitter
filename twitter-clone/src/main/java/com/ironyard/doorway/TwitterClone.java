5c5e2eeaf1232b80e799b3ecb38578307b4b33d4
paulgfrey <paulgfrey@gmail.com>
2016-08-08 10:31:49 -0400

    fixed static content

--- twitter-clone/src/main/java/com/ironyard/doorway/TwitterClone.java
+++ twitter-clone/src/main/java/com/ironyard/doorway/TwitterClone.java
@@ -1,19 +1,9 @@
 package com.ironyard.doorway;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.server.handler.DefaultHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
-import org.eclipse.jetty.util.resource.Resource;
 
 import com.ironyard.doorway.handlers.MyResourceHandler;
 import com.ironyard.doorway.handlers.RestAPIHandler;
@@ -25,8 +15,7 @@ public class TwitterClone {
         Server server = new Server(port);
         
         HandlerList handlers = new HandlerList();
-        MyResourceHandler resourceHandler = new MyResourceHandler();
-        resourceHandler.setBaseResource(Resource.newResource("./src/main/webapp"));
+        MyResourceHandler resourceHandler = new MyResourceHandler("./src/main/webapp");
         handlers.setHandlers(new Handler[]
         { resourceHandler, new RestAPIHandler(), 
         		new ServletHandler(), new DefaultHandler() });
--- twitter-clone/src/main/java/com/ironyard/doorway/handlers/MyResourceHandler.java
+++ twitter-clone/src/main/java/com/ironyard/doorway/handlers/MyResourceHandler.java
@@ -1,5 +1,8 @@
 package com.ironyard.doorway.handlers;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -7,23 +10,70 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.server.handler.AbstractHandler;
 
-public class MyResourceHandler extends ResourceHandler {
+public class MyResourceHandler extends AbstractHandler {
+	String path = ".";
 	
-	public MyResourceHandler() {
+	public MyResourceHandler(String path) {
 		super();
+		this.path = path;
 	}
 
 	@Override
 	public void handle(String target, Request baseRequest, HttpServletRequest request,
 			HttpServletResponse response) throws IOException, ServletException {
+		if(target != null) {
+			if(! (target.endsWith(".html") ||
+					target.endsWith("htm") || 
+					target.endsWith(".css") ||
+					target.endsWith(".js") ||
+					target.endsWith(".jpg") ||
+					target.endsWith(".png"))) {
+				return;
+			}
+		}
+		System.out.println("target=" + target);
 		// Get cookie if present
 		String userId = Utils.getUserId(request);
 		if(userId.length() == 0) {
 			userId = "User";
 		}
-		super.handle(target, baseRequest, request, response);
+		File file = new File(path + target);
+		if(file.exists()) {
+			response.setContentType("text/html;charset=utf-8");
+			if(target.endsWith(".css")) {
+				response.setContentType("text/css;charset=utf-8");
+			}
+			else if(target.endsWith(".jpg")) {
+				response.setContentType("image/jpeg");
+			}
+			else if(target.endsWith(".png")) {
+				response.setContentType("image/png");
+			}
+			else if(target.endsWith(".js")) {
+				response.setContentType("application/javascript");
+			}
+			FileReader fr = new FileReader(file);
+			try {
+				BufferedReader br = new BufferedReader(fr);
+				String line;
+				while((line = br.readLine()) != null) {
+					response.getWriter().println(line);
+				}
+				response.setStatus(HttpServletResponse.SC_OK);
+				baseRequest.setHandled(true);
+			}
+			catch(IOException e) {
+				e.printStackTrace(System.err);
+				return;
+			}
+			finally {
+				if(fr != null) {
+					fr.close();
+				}
+			}
+		}
 		// Now tack on Cookie
 		Utils.setUserId(response, userId);
 	}
